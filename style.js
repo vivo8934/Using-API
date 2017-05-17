b72e9b1eee4d698f3ba64e44796fdaeabf84b8b5
vivo8934 <mfundo8934@gmail.com>
2017-05-17 12:55:16 +0200

    show all the repos on the empty div

--- style.js
+++ style.js
@@ -5,11 +5,13 @@ var Output = document.querySelector('.output');
 
 myBtn.addEventListener('click', function(){
 
-  var url = "https://api.github.com/users/" + textarea.value;
+
+  var url = "https://api.github.com/users/" + textarea.value+ "/repos";
 
   $.get(url)
-  .then(function(result){
-Output.innerHTML =result;
+    .then(function(result){
 
-  })
+       var message = "Hi, " + textarea.value + " you have " + JSON.stringify(result) + " repos in GitHub!"
+       Output.innerHTML = message;
+   });
 })
